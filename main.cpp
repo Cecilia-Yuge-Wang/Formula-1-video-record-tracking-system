ad744338e903edd818425aef4f03302f056e7f67
Ranger5120 <1269883269@qq.com>
2023-03-31 18:15:24 +0100

    main function of motor control, used for testing

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
-//this is the main function of the project
+#include<wiringPi.h>
+#include<softPwm.h>
+#include<iostream>
+#include "motor.h"
+
+#define motor1 17 //vertical
+#define motor2 27 //horizontal
+
+using namespace std;
+
+int main()
+{
+    motor verticalMotor(motor1);
+    motor horizontalMotor(motor2);
+
+    while (true)
+    {
+        char choice = getchar();
+    
+        switch (choice)
+        {
+            case 'a':
+                //turning left 
+                horizontalMotor.clockwiseRotate();
+                break;
+            
+            case 'd':
+                //turning right
+                horizontalMotor.antiClockRotate();
+                break;
+            
+            case 'w':
+                //turning up
+                verticalMotor.clockwiseRotate();
+                break;
+            
+            case 's':
+                //turning down
+                verticalMotor.antiClockRotate();
+                break;
+            
+            default:
+                break;
+        }
+    }
+    
+    return 0;
+}
