76457d2990b623106ee1526e9cd6516137615633
WilliamAND1 <yu963354823@gmail.com>
2023-04-10 18:47:09 +0100

    Class for Control

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,29 +1,26 @@
 #include"Controller.h"
 #include"motor.h"
 
-Controller::Controller(int motor1,int motor2):Vertical(motor1),Horizontal(motor2){};
+Controller::Controller(int motor1,int motor2):Vertical(motor1),Horizontal(motor2){
+
+};
 
 void Controller::ServoControl(float image_y,float image_x)
 {
-    if(image_y>380 && image_x>660){         //Direction: Bottom Right
-        Horizontal.antiClockRotate();       //right
-        delay(50);
+    if(image_y>520){
         Vertical.clockwiseRotate();         //down
-        delay(50);
-    }else if(image_y>380 && image_x<620){   //Direction: Bottom Left
-        Horizontal.clockwiseRotate();       //left
-        delay(50);
-        Vertical.clockwiseRotate();         //down
-        delay(50);
-    }else if(image_y<340 && image_x>660){   //Direction: Top Right
-        Horizontal.antiClockRotate();       //right
-        delay(50);
+        delay(100);
+    }else if(image_y<200){
         Vertical.antiClockRotate();         //up
-        delay(50);
-    }else if(image_y<340 && image_x<620){   //Direction: Top Left
-        Horizontal.clockwiseRotate();       //left
-        delay(50);
-        Vertical.antiClockRotate();         //up
-        delay(50);
+        delay(100);
     }
+    
+    if(image_x>880){
+        Horizontal.antiClockRotate();       //right
+        delay(100);
+    }else if(image_x<400){
+        Horizontal.clockwiseRotate();       //left
+        delay(100);
+    }
+    
 }
