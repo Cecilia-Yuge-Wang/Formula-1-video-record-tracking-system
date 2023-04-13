e74c4e2945e4fac2f99ba54c333fc690e4f323ca
Minghao Xuan <42275133+Ranger5120@users.noreply.github.com>
2023-04-13 21:45:29 +0100

    Minghao branch (#46)
    * delete the servo thread method
    
    * return
    
    * fixing bug of thread not activated

--- test/detectionTest/demo_detect.cpp
+++ test/detectionTest/demo_detect.cpp
@@ -21,6 +21,7 @@ int main()
         return -1;
     }
 
+    controller.controlThread();
     std::thread t1(&CNN::processThread, &api, std::ref(cap));
     
     cv::Mat cvImg;
@@ -41,10 +42,6 @@ int main()
     	int y = api.getY();
     	std::cout<<"x = "<< x << std::endl;
         controller.getCoordinate(y, x);
-        // controller.controlThread();
-
-
-
 	
         cv::imshow("Camera", cvImg); // ��ʾ����ͷ����
         if (cv::waitKey(1) == 27) { // ����Esc���˳�ѭ��
