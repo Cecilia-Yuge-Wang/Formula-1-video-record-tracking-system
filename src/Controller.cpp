621f1e16b3d751219d7db27cea9815ecac0c69f8
Ranger5120 <1269883269@qq.com>
2023-04-14 16:47:24 +0100

    some adjustment in Controller class

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -3,6 +3,7 @@
 #include<thread>
 #include<iostream>
 #include<time.h>
+#include<chrono>
 
 
 
@@ -11,12 +12,6 @@ Controller::Controller(int motor1,int motor2):Vertical(motor1),Horizontal(motor2
         std::cout<<"pantilt is initialized"<<std::endl;
 };
 
-void Controller::controlThread()
-{
-    std::thread pantiltThread(&Controller::control, this);
-    pantiltThread.detach();
-}
-
 //in this function, the central coordinate of the object will be compared with the center area of the screen. if the coordiante is much far 
 //away from the central area, the servo class will be called to perform the rotate to make the pantilt tracking the object.
 
@@ -24,20 +19,21 @@ void Controller::ServoControl(float image_y,float image_x)
 {
     if(image_y>520){
         Vertical.clockwiseRotate();         //down
-        delay(100);
+        //delay(100);
     }else if(image_y<200){
         Vertical.antiClockRotate();         //up
-        delay(100);
+        //delay(100);
     }
     
     if(image_x>880){
         Horizontal.antiClockRotate();       //right
-        delay(100);
+        //delay(100);
     }else if(image_x<400){
         Horizontal.clockwiseRotate();       //left
-        delay(100);
+        //delay(100);
     }
     
+
 }
 
 //this function is a callback function to get the coordiante of the object central from frameprocess class.
@@ -48,6 +44,8 @@ void Controller::getCoordinate(float y, float x)
     //std::cout<<"the coordinate received in class is: x = "<<image_x<<" y = "<<image_y<<std::endl;
 }
 
+
+//this function is used to print the coordinate input from get function for testing
 void Controller::printCoordinate()
 {
     std::cout<<"x = " <<image_x<<", y = "<<image_y<<std::endl;
@@ -56,14 +54,24 @@ void Controller::printCoordinate()
 void Controller::control()
 {
     int deter_x = 0;
-
+    std::chrono::milliseconds dura( 100 );
+    
     while (true)
     {
-         if (deter_x != image_x){
+        if (deter_x != image_x)
+        {
             ServoControl(image_y, image_x);
         }
-        
+
         deter_x = image_x;
+        std::this_thread::sleep_for( dura );
     }
     
+}
+
+
+void Controller::controlThread()
+{
+    std::thread pantiltThread(&Controller::control, this);
+    pantiltThread.detach();
 }
