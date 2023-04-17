aaffe04dd24b8cba259aadf8041e46fbf987ede8
Cecilia-Yuge-Wang <2809017W@student.gla.ac.uk>
2023-04-17 23:47:30 +0100

    Supplementary models

--- test/detectionTest/src/include/frameprocess.h
+++ test/detectionTest/src/include/frameprocess.h
@@ -1,12 +1,9 @@
 #ifndef NCNN_H_
 #define NCNN_H_
-
 #include "net.h"
-
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <atomic>
-
 #include <fstream>
 #include <string>
 
@@ -43,7 +40,7 @@ private:
     int numThreads;
     int numCategory;
     int inputWidth, inputHeight;
-    
+
     int rx;
     int ry;
     static const char** class_names;
@@ -52,27 +49,27 @@ private:
 
     int nmsHandle(std::vector<TargetBox> &tmpBoxes, std::vector<TargetBox> &dstBoxes);
     int getCategory(const float *values, int index, int &category, float &score);
-    int predHandle(const ncnn::Mat *out, std::vector<TargetBox> &dstBoxes, 
+    int predHandle(const ncnn::Mat *out, std::vector<TargetBox> &dstBoxes,
                    const float scaleW, const float scaleH, const float thresh);
 
 public:
     CNN();
     ~CNN();
-    
+
     cv::Mat srcImg;
     std::atomic<bool> g_quit{false};
-    
+
     void read_class_names(const char* filename);
-    
+
     int loadModel(const char* paramPath, const char* binPath);
-    int detection(const cv::Mat srcImg, std::vector<TargetBox> &dstBoxes, 
+    int detection(const cv::Mat srcImg, std::vector<TargetBox> &dstBoxes,
                   const float thresh = 0.5);
-    void processThread(cv::VideoCapture& cap); 
+    void processThread(cv::VideoCapture& cap);
     void rectangle(const cv::Mat srcImg, std::vector<TargetBox> &dstBoxes);
     void showThread(cv::Mat& img);
-    
+
     int getX() const { return rx; }
     int getY() const { return ry; }
-    
+
 };
 #endif
